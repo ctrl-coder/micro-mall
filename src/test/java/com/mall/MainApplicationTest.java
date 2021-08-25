368b213e831f4aa794dd40b501ae4fe0f2d34d18
yuntianwen <yuntian.wen1@thoughtworks.com>
2021-08-26 01:30:14 +0800

    integrate junit5

---
+++ src/test/java/com/mall/MainApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mall;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainApplicationTest {
+
+    @Test
+    void main() {
+        assertEquals(10, 10);
+    }
+}
